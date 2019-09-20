36916a5d4b792a952543234264f4ed57fdb7d63a
adamc <adamc@DESKTOP-BGFEHG7>
2019-09-21 01:28:28 +0200

    Tworzenie modelu i repozytorium

---
+++ Komis/Models/ISamochodReposiotory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Komis.Models
+{
+    public interface ISamochodReposiotory
+    {
+        IEnumerable<Samochod> PobierzWszystkieSamochody();
+        Samochod PobierzSamochodPoId(int samochodId);
+    }
+}
