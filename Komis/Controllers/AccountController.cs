5db6a7388dcf48c6cd930fa95eba947c3f04faf3
adamc <adamc@DESKTOP-BGFEHG7>
2019-09-23 06:46:31 +0200

    Autoryzowanie użytkowników

--- Komis/Controllers/AccountController.cs
+++ Komis/Controllers/AccountController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Komis.ViewModels;
+﻿using Komis.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Komis.Controllers
 {
